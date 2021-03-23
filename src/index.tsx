b8466374e149bce49da12eb2a2cb040bcbdcccc3
Barto <illya1004work@gmail.com>
2021-03-23 09:48:48 +0200

    settings iframe sandbox

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import {fetchPlugin} from "./plugin/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>()
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -26,6 +26,9 @@ const App = () => {
         if (!ref.current) {
             return;
         }
+
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -39,8 +42,30 @@ const App = () => {
                 global: 'window'
             }
         })
-        setCode(result.outputFiles[0].text)
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     }
+
+    const html = `
+<html>
+<head></head>
+<body>
+<div id="root"></div>
+<script>
+window.addEventListener('message', (evt) => {
+  try {
+    eval(evt.data);
+  } catch (err) {
+    const root = document.querySelector('#root');
+    root.innerHTML = '<div style="color:red;"><h4>' + err + '</h4></div>';
+    console.error(err);
+  }
+  
+  }, false)
+</script>
+</body>
+</html>
+`;
 
     return (
         <div>
@@ -48,9 +73,16 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const App = () => <h1>Hi Illia</h1>;
+//
+// ReactDOM.render(<App/>, document.querySelector('#root'))
