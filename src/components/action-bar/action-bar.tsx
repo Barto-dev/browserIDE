a04428f15d6878408c527b1bbea9f16c6856913a
Barto <illya1004work@gmail.com>
2021-04-05 10:08:54 +0300

    action bar styling

--- src/components/action-bar/action-bar.tsx
+++ src/components/action-bar/action-bar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useActions} from "../../hooks/use-action";
 import ActionButton from "../action-button/action-button";
+import './action-bar.css';
 
 interface ActionBarProps {
     id: string;
@@ -9,7 +10,7 @@ interface ActionBarProps {
 const ActionBar: React.FC<ActionBarProps> = ({id}) => {
     const {moveCell, deleteCell} = useActions();
     return (
-        <div>
+        <div className="action-bar">
             <ActionButton icon="fa-arrow-up" onClick={() => moveCell(id, 'up')} />
             <ActionButton icon="fa-arrow-down" onClick={() => moveCell(id, 'down')} />
             <ActionButton icon="fa-times" onClick={() => deleteCell(id)} />
