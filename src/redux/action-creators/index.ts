0ce7d51d493bef68a7caff4c98f63a185bcb4c30
Barto <illya1004work@gmail.com>
2021-04-15 22:53:44 +0300

    cells reducer

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,8 +1,11 @@
-import {ActionType} from "../action-types";
 import {Dispatch} from "redux";
+
+import {ActionType} from "../action-types";
 import {Action, DeleteCellAction, Direction, InsertCellAfterAction, MoveCellAction, UpdateCellAction} from "../actions";
-import {CellTypes} from "../cell";
+import {Cell, CellTypes} from "../cell";
 import bundle from "../../bundler";
+import axios from "axios";
+import {RootState} from "../reducers";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
     return {
@@ -47,4 +50,33 @@ export const createBundle = (cellId: string, input: string) => async (dispatch: 
             bundle: {code, err}
         }
     })
+}
+
+export const fetchCells = () => async (dispatch: Dispatch<Action>) => {
+    dispatch({type: ActionType.FETCH_CELLS});
+    try {
+        const {data}: { data: Cell[] } = await axios.get('/cells');
+        dispatch({
+            type: ActionType.FETCH_CELLS_COMPLETE, payload: data
+        })
+    } catch (err) {
+        dispatch({
+            type: ActionType.FETCH_CELLS_ERROR,
+            payload: err.message
+        })
+    }
+}
+
+export const saveCells = () => async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {cells: {data, order}} = getState();
+    const cells = order.map(id => data[id]);
+    try {
+        await axios.post('/cells', {cells})
+    } catch (err) {
+        dispatch({
+            type: ActionType.SAVE_CELLS_ERROR,
+            payload: err.message
+        })
+    }
+
 }
